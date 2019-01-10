b396ae0a5ed3930883ae51321e77c1c3ad41df98
Jehu <jehucjp@gmail.com>
2019-01-10 13:48:24 -0500

    -imported bootswatch theme -created fragments -created userService

--- Practice/src/main/java/com/persistance/repository/UserRepository.java
+++ Practice/src/main/java/com/persistance/repository/UserRepository.java
@@ -10,9 +10,9 @@ import com.persistance.models.Users;
 public interface UserRepository extends JpaRepository<Users, Integer>{
 
 	Optional <Users> findByUsername(String username);
+	Users findOne(String email);
 	// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-	// CRUD refers Create, Read, Update, Delete
-	
-	// interacting with the repository will interact with database in our stead
-	// we deal with the DB at the java level, SPRING takes care of persistence
+	// CRUD refers Create, Read, Update, Delete	
+	/* interacting with the repository will interact with database in our stead
+	   we deal with the DB at the java level, SPRING takes care of persistence */
 }
---
+++ Practice/src/main/java/com/persistance/service/UserService.java
@@ -0,0 +1,51 @@
+/* service classes are the ones that make use of the repositories to implement 
+ * business logic... it aims to separate the UI logic and the business one*/
+package com.persistance.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.persistance.models.Role;
+import com.persistance.models.Users;
+import com.persistance.repository.UserRepository;
+
+@Service /*this class will be created as a bean and placed in the application context*/
+public class UserService {
+	@Autowired // autowiring injects the object dependency implicitly... internally uses setter 
+	//or constructor injection so no need for objectType variableName = new objectType();
+	private UserRepository userRepository; 
+	
+	// method to insert user for registration...
+	public void createUser(Users user) {
+		
+		BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder();
+		user.setPassword(encoder.encode(user.getPassword()));
+	
+		Role userRole = new Role("USER"); 
+		List<Role> roles = new ArrayList<>();
+		roles.add(userRole);
+		user.setRoles(roles); 
+		userRepository.save(user);		
+	}
+	// only the method above is documented... it's the same code different method name and string argument
+	public void createAdmin(Users user) {
+		
+		BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder();
+		user.setPassword(encoder.encode(user.getPassword()));
+	
+		Role userRole = new Role("Admin"); 
+		List<Role> roles = new ArrayList<>();
+		roles.add(userRole);
+		user.setRoles(roles); 
+		userRepository.save(user);	
+	}
+	
+	public Users findOne(String email) {
+		return userRepository.findOne(email);
+	}
+	
+}
