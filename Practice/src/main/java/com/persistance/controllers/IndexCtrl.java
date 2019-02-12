4af4abbb0b2d8d3ea197fda6a876b98f3aafe184
Jehu <jehucjp@gmail.com>
2019-02-12 12:31:26 -0500

    new database

--- Practice/src/main/java/com/persistance/controllers/IndexCtrl.java
+++ Practice/src/main/java/com/persistance/controllers/IndexCtrl.java
@@ -2,17 +2,16 @@ package com.persistance.controllers;
 
 import java.security.Principal;
 
-import javax.servlet.http.HttpSession;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.persistance.models.Users;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
 import com.persistance.service.UserService;
 
 @Controller
